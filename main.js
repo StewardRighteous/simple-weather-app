65f63509d5f07456ecee6fad4fc96e56cb0ddca1
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2024-12-21 10:53:34 +0530

    show celsius of location when clicking search  button

--- main.js
+++ main.js
@@ -1,17 +1,42 @@
-let locationName = prompt("Enter a Location");
 const APIKEY = "EKXD36QWFM5NSGPP8S57846J4";
-let currentWeather;
+const searchButton = document.querySelector("button");
+const locationInput = document.querySelector("input#location");
+const showWeatherHeading = document.querySelector("h1");
 
-async function getWeather() {
+function convertToCelsius(fahrenheit) {
+  let celsius;
+  celsius = ((fahrenheit - 32) * 5) / 9;
+  return celsius;
+}
+
+async function showWeather(currentLocation) {
+  let weatherData = await getWeatherData(currentLocation);
+  let weather = `${convertToCelsius(weatherData.currentConditions.temp)}`;
+  showWeatherHeading.textContent = weather;
+}
+
+async function getWeatherData(location) {
   try {
-    let fetchWeatherDetails = await fetch(
-        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationName}?key=${APIKEY}`
-    );
-    let weatherDetails = await fetchWeatherDetails.json();
-    console.log(weatherDetails);
+    let link = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${APIKEY}`;
+    let fetchWeatherDetails = await fetch(link, { mode: "cors" });
+
+    if (!fetchWeatherDetails.ok) {
+      console.log("Error");
+    } else {
+      let weatherDetails = await fetchWeatherDetails.json();
+      console.log(weatherDetails);
+      return weatherDetails;
+    }
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
 }
 
-getWeather();
+searchButton.addEventListener("click", (e) => {
+  e.preventDefault(); // Stops reloading of page when the button is clicked (reload erases async details and causes failures )
+  if (locationInput.value.length == 0) {
+    alert("Enter Location");
+  } else {
+    showWeather(locationInput.value);
+  }
+});
