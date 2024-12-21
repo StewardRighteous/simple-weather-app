df97a142e9dbfe4f81889fab00f0aace4c00f84f
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2024-12-21 12:25:01 +0530

    show weather details in UI

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const APIKEY = "EKXD36QWFM5NSGPP8S57846J4";
 const searchButton = document.querySelector("button");
 const locationInput = document.querySelector("input#location");
-const showWeatherHeading = document.querySelector("h1");
+const weatherDetailsContainer = document.querySelector(".weather-details");
+const weatherImage = document.querySelector("img");
+const weatherLocationHeader = document.querySelector("h1");
+const weatherTemp = document.querySelector("h2");
+const weatherDescription = document.querySelector("h3");
+const isCelsiusCheckbox = document.querySelector("input#celsius-fahrenheit");
 
 function convertToCelsius(fahrenheit) {
   let celsius;
@@ -9,10 +14,25 @@ function convertToCelsius(fahrenheit) {
   return celsius;
 }
 
-async function showWeather(currentLocation) {
+// Shows weather details in UI
+async function showWeatherInContainer(currentLocation) {
   let weatherData = await getWeatherData(currentLocation);
-  let weather = `${convertToCelsius(weatherData.currentConditions.temp)}`;
-  showWeatherHeading.textContent = weather;
+  console.log(weatherData);
+  weatherLocationHeader.textContent = `${weatherData.resolvedAddress}`;
+  weatherDescription.textContent = `${weatherData.currentConditions.conditions}`;
+
+  let icon = `./icons/${weatherData.currentConditions.icon}.svg`;
+  weatherImage.src = icon;
+
+  if (isCelsiusCheckbox.checked) {
+    weatherTemp.textContent = `${Math.round(
+      convertToCelsius(weatherData.currentConditions.temp)
+    )} ℃`;
+  } else {
+    weatherTemp.textContent = `${Math.round(
+      weatherData.currentConditions.temp
+    )} ℉`;
+  }
 }
 
 async function getWeatherData(location) {
@@ -24,11 +44,10 @@ async function getWeatherData(location) {
       console.log("Error");
     } else {
       let weatherDetails = await fetchWeatherDetails.json();
-      console.log(weatherDetails);
       return weatherDetails;
     }
   } catch (err) {
-    console.error(err);
+    alert(`Error : ${new Error(err)}`);
   }
 }
 
@@ -37,6 +56,8 @@ searchButton.addEventListener("click", (e) => {
   if (locationInput.value.length == 0) {
     alert("Enter Location");
   } else {
-    showWeather(locationInput.value);
+    showWeatherInContainer(locationInput.value);
   }
 });
+
+isCelsiusCheckbox.addEventListener("change", );
